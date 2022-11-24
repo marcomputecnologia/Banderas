487ab1d739219d3466f85be9c843c214c84991c2
marcomputecnologia <marcomputecnologia@gmail.com>
2022-11-23 19:44:34 -0600

    finalizacion de banderas

--- js/script.js
+++ js/script.js
@@ -1,10 +1,44 @@
+function Paises(elemento){
+    if(elemento == "ejercicio1"){
+        document.getElementById('ejercicio1').style.display = 'block';
+        document.getElementById('ejercicio2').style.display = 'none';
+        document.getElementById('ejercicio3').style.display = 'none';
+        document.getElementById('ejercicio4').style.display = 'none';
+        document.getElementById('ejercicio5').style.display = 'none';  
+    }
 
+    else if(elemento == "ejercicio2"){
+        document.getElementById('ejercicio2').style.display = 'block';
+        document.getElementById('ejercicio1').style.display = 'none';
+        document.getElementById('ejercicio3').style.display = 'none';
+        document.getElementById('ejercicio4').style.display = 'none';
+        document.getElementById('ejercicio5').style.display = 'none';
+    }
 
-function Banderas_paises() {
-    document.getElementById('ejercicio1').style.display = 'block'
+    else if(elemento == "ejercicio3"){
+        document.getElementById('ejercicio3').style.display = 'block';
+        document.getElementById('ejercicio1').style.display = 'none';
+        document.getElementById('ejercicio2').style.display = 'none';
+        document.getElementById('ejercicio4').style.display = 'none';
+        document.getElementById('ejercicio5').style.display = 'none';
+    }
+
+    else if(elemento == "ejercicio4"){
+        document.getElementById('ejercicio4').style.display = 'block';
+        document.getElementById('ejercicio1').style.display = 'none';
+        document.getElementById('ejercicio2').style.display = 'none';
+        document.getElementById('ejercicio3').style.display = 'none';
+        document.getElementById('ejercicio5').style.display = 'none';
+    }
+
+    else if (elemento=="ejercicio5"){
+        document.getElementById("ejercicio5").style.display="block";
+        document.getElementById('ejercicio1').style.display = 'none';
+        document.getElementById('ejercicio2').style.display = 'none';
+        document.getElementById('ejercicio3').style.display = 'none';
+        document.getElementById('ejercicio4').style.display = 'none';
+    }
+
 }
-
-
-
 
 
